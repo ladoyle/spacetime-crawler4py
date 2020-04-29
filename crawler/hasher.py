66dc1bbcdd668e2e84902625e4532cafc3155568
haelfeyn <healfein@gmail.com>
2020-04-29 13:24:32 -0700

    - reverted weighting to original scheme using non-modified frequencies - changed similarity checking to use hamming distance (counting ratio of differing bits)

--- crawler/hasher.py
+++ crawler/hasher.py
@@ -10,13 +10,11 @@ class SimHash:
     @staticmethod
     def create_fingerprint(page_content):
         hex_scale = 16
-        # num_words = 0
         ini_hashes = []
         page_words = []
         vector_values = [0] * 160
         # hash all words
         for word in page_content:
-            # num_words += page_content[word]
             page_words.append(word)
             hash_hex = sha1(word.encode('utf-8')).hexdigest()
             hash_bin = bin(int(hash_hex, hex_scale))
@@ -25,11 +23,10 @@ class SimHash:
         for i in range(len(ini_hashes)):
             hash_bin = ini_hashes[i]
             for j in range(len(hash_bin)):
-                # weight = num_words - page_content[page_words[i]]
                 if hash_bin[j] == '0':
-                    vector_values[j] -= page_content[page_words[i]]  # weight
+                    vector_values[j] -= page_content[page_words[i]]
                 else:
-                    vector_values[j] += page_content[page_words[i]]  # weight
+                    vector_values[j] += page_content[page_words[i]]
         # build new fingerprint
         fingerprint = b''
         for value in vector_values:
@@ -37,16 +34,25 @@ class SimHash:
                 fingerprint += b'1'
             else:
                 fingerprint += b'0'
-        return int(fingerprint, 2)
+        return fingerprint
 
     def calculate_similarity(self, new_hash):
         # calculate the similarity of each page from the previous fingerprints
         similarity = 0
         for fingerprint in self.fingerprints:
-            similarity = max(similarity, min(new_hash, fingerprint) / max(new_hash, fingerprint))
+            similarity = max(similarity, self.hamming_distance(fingerprint, new_hash))
             if similarity > 0.90:
                 return similarity
         return similarity
+
+    @staticmethod
+    def hamming_distance(fingerprint, new_hash):
+        distance = 0
+        size = len(fingerprint)
+        for i in range(size):
+            if fingerprint[i] != new_hash[i]:
+                distance += 1
+        return distance / size
 
     def save_fingerprint(self, fingerprint):
         self.fingerprints.append(fingerprint)
